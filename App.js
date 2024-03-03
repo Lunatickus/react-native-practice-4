fa2cc25e6bdf803b79d8374cced2332ec188bc88
Lunatickus <125456338+Lunatickus@users.noreply.github.com>
2024-03-03 13:01:39 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,35 @@
+import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+import {
+  Keyboard,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import { RegistrationScreen } from "./components/RegistrationScreen";
+import { LoginScreen } from "./components/LoginScreen";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Light": require("./assets/fonts/Roboto-Light.ttf"),
+  });
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        {/* <LoginScreen /> */}
+        <RegistrationScreen />
+        <StatusBar style="auto" />
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
